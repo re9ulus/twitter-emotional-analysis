bf4fe4f4f6d3c82b773e33d77de535716b587d00
Basil Zemlyanov <angeldaren@gmail.com>
2013-09-18 15:33:59 +0300

    Refactor: twiiter_emotional

--- twitter_emotional.rb
+++ twitter_emotional.rb
@@ -1,22 +1,34 @@
 class TwitterEmotional
-	
-	def initialize
-		@word_pattern = /\b(\w+)\b/
+
+	RATED_WORDS_FILE_PATH = 'AFFIN-111.txt'
+
+	def initialize()
+		@rated_words = {}
+		File.open(RATED_WORDS_FILE_PATH).each do |line|
+			word, rate = line.split
+			# HACK: removes expressions from 2 or more words.
+			# TODO: process multiple words
+		  rate = Integer(rate) rescue nil 
+			@rated_words[word.strip] = Integer(rate) if rate 
+		end
 	end
 
-	def word_emotion(word)
-		1
+	def rate_word(word)
+		if @rated_words.include? word
+			@rated_words[word]
+		else
+			puts word + '+'
+			0
+		end
 	end
 
 	def words_from_tweet(tweet)	
-		tweet.scan(@word_pattern)
+		tweet.downcase.split(/\W+/)
 	end
 
-	def tweet_emotion(tweet)
-		words = words_from_tweet tweet
-		res = 0
-		words.each { |word| res += word_emotion word }
-		res
+	def rate_tweet(tweet)
+		rates = words_from_tweet(tweet).map { |word| rate_word(word) }
+		rates.inject(:+)
 	end
 
 end
